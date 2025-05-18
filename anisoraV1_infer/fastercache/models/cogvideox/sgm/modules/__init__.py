4b30dbd7bcda579449fde67613d4fbb607194c3f
xunmengshe2x <xumengshe@gmail.com>
2025-05-18 15:51:36 +0100

    Clone anisoraV1_infer/fastercache/models/cogvideox/sgm/modules/__init__.py from bilibili/Index-anisora

---
+++ anisoraV1_infer/fastercache/models/cogvideox/sgm/modules/__init__.py
@@ -0,0 +1,6 @@
+from .encoders.modules import GeneralConditioner
+
+UNCONDITIONAL_CONFIG = {
+    "target": "fastercache.models.cogvideox.sgm.modules.GeneralConditioner",
+    "params": {"emb_models": []},
+}
