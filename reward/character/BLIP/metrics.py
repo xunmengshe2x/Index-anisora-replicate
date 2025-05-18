1566e8de94d354b5ef46248c16c45c57d62d906b
xunmengshe2x <xumengshe@gmail.com>
2025-05-18 17:33:25 +0100

    Clone reward/character/BLIP/metrics.py from bilibili/Index-anisora

---
+++ reward/character/BLIP/metrics.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def eval_func(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=50):
+    """
+        Evaluation with market1501 metric
+    """
+    num_q, num_g = distmat.shape
+    if num_g < max_rank:
+        max_rank = num_g
+        print("Note: number of gallery samples is quite small, got {}".format(num_g))
+    indices = np.argsort(distmat, axis=1)[:, ::-1]
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+    all_cmc = []
+    all_AP = []
+    num_valid_q = 0.  # number of valid query
+    for q_idx in range(num_q):
+        q_pid = q_pids[q_idx]
+        q_camid = q_camids[q_idx]
+
+        order = indices[q_idx]  # select one row
+        remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+        keep = np.invert(remove)
+
+        orig_cmc = matches[q_idx][keep]
+        if not np.any(orig_cmc):
+            continue
+
+        cmc = orig_cmc.cumsum()
+        cmc[cmc > 1] = 1
+
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+        num_rel = orig_cmc.sum()
+        tmp_cmc = orig_cmc.cumsum()
+        y = np.arange(1, tmp_cmc.shape[0] + 1) * 1.0
+        tmp_cmc = tmp_cmc / y
+        tmp_cmc = np.asarray(tmp_cmc) * orig_cmc
+        AP = tmp_cmc.sum() / num_rel
+        all_AP.append(AP)
+
+    assert num_valid_q > 0, "Error: all query identities do not appear in gallery"
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mAP = np.mean(all_AP)
+
+    return all_cmc, mAP
