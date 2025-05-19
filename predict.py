dce4e8476cacd7e78ab38f62d362a74046b64bde
xunmengshe2x <xumengshe@gmail.com>
2025-05-19 10:46:55 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -50,7 +50,15 @@ class Predictor(BasePredictor):
                 torch_dtype=torch.bfloat16,
                 use_safetensors=True
             )
-            self.config = OmegaConf.load("anisoraV1_infer/configs/cogvideox/cogvideox_5b_720_169_2.yaml")
+            #self.config = OmegaConf.load("anisoraV1_infer/configs/cogvideox/cogvideox_5b_720_169_2.yaml")
+
+            from cogvideox.pipeline_cogvideox_image2video import CogVideoXConfig
+            config = CogVideoXConfig(
+                model_path=model_id,
+                num_gpus=1,  # Adjust based on your setup
+                cpu_offload=False,
+                vae_tiling=True
+            )
             # Download transformer
             self.pipe = CogVideoXImageToVideoPipeline.from_pretrained(
                 model_id,
@@ -58,7 +66,7 @@ class Predictor(BasePredictor):
                 text_encoder=text_encoder,
                 tokenizer=tokenizer,
                 torch_dtype=torch.bfloat16,
-                config=self.config
+                config=config
             )
 
             # Load config
